984d6cc0e90ada9ff6b493774a610cf2ecb0c9ab
Titanovsky <titanovskyteam@gmail.com>
2024-03-03 02:23:27 +0500

    test array TestJson for SaveData.Save

---
+++ code/Achievement/AchievementComponent.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+public sealed class AchievementComponent : Component
+{
+	protected override void OnStart()
+	{
+		TestJson[] objs = { new TestJson(123, true, "dsads"), new TestJson(999, false, "dasdsdsaadsds") };
+
+		SaveData.Save("slot.json", objs);
+	}
+}
