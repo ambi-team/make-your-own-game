0a6bb5b436c88969fc47648b75daf26f0d4506b2
Titanovsky <titanovskyteam@gmail.com>
2024-03-02 17:46:03 +0500

    add MainMenu logic + razor

--- code/MainMenu/MainMenu.cs
+++ code/MainMenu/MainMenu.cs
@@ -1,12 +1,39 @@
-﻿using System;
+﻿using Sandbox.UI;
+using System;
 
 public sealed class MainMenu : Component
 {
+	[Property] public ScreenPanel pnl;
+
+	private bool isShow = false;
+
+	public void Show()
+	{
+		if (isShow)
+		{
+			Remove(); 
+
+			return;
+		}
+
+		isShow = true;
+		pnl.Enabled = true;
+	}
+
+	public void Remove()
+	{
+		isShow = false;
+		pnl.Enabled = false;
+	}
+
 	protected override void OnUpdate()
 	{
-		if (Input.Pressed("use"))
-		{
-			//todo
-		}
+		if (Input.EscapePressed)
+			Show();
+	}
+
+	protected override void OnStart()
+	{
+		pnl.Enabled = false;
 	}
 }
