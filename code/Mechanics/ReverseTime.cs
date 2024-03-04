746becf585e0ba829ee9ccaa2e046c46bfb6c1ee
Titanovsky <titanovskyteam@gmail.com>
2024-03-04 08:53:32 +0500

    add raw logic ReverseTime

---
+++ code/Mechanics/ReverseTime.cs
@@ -0,0 +1,9 @@
+﻿public sealed class ReverseTime : Component
+{
+	[Property] public GameObject ply;
+
+	//todo 1. Сделать OnRecordInputsPlayers
+	//todo 2. Записывать действия игрока 20 секунд (ключ = единица времени; значение = инпут) 
+	//todo 3. Переделать PlayerController
+	//todo 4. Воспроизвести + не забыть про предметы
+}
