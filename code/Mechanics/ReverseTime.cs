b676904452f1b8cbefa887d99de50d8da2909b54
Titanovsky <titanovskyteam@gmail.com>
2024-03-05 15:52:21 +0500

    add Record Logic and formatting code

--- code/Mechanics/ReverseTime.cs
+++ code/Mechanics/ReverseTime.cs
@@ -4,57 +4,159 @@ public sealed class ReverseTime : Component
 {
 	[Property] public GameObject ply;
 
-	private bool onRecording;
+	public bool onRecording;
+	public bool onPlaying;
+
 	private int index = 0;
-	private float time = 20f;
+
+	private Vector3 startPos;
+	//todo startEyeAngles
+	private float defaultTimeRecoding = 5f;
 	private TimeUntil TimeRecording { get; set; }
 
 	public Dictionary<float, string> actions = new();
+	//todo record mouse axis
 
-	//todo 1. Сделать OnRecordInputsPlayers
-	//todo 2. Записывать действия игрока 20 секунд (ключ = единица времени; значение = инпут) 
 	//todo 3. Переделать PlayerController
 	//todo 4. Воспроизвести + не забыть про предметы
 
-	public void Record()
+	#region Record logic
+	public void StartRecord()
 	{
 		if (onRecording) return;
+		if (onPlaying) return;
 
+		if (actions.Count > 0)
+		{
+			for (int i = 0; i < actions.Count; i++)
+				actions[i] = "";
+		}
 		index = 0;
-		TimeRecording = time;
-		onRecording = true;
+		TimeRecording = defaultTimeRecoding;
+		onRecording = true; // native start
 
-		Log.Info($"[ReverseTime] Record");
+		Log.Info($"[ReverseTime] Start {index}");
 	}
 
-	public void Stop()
-	{
-		if (!onRecording) return;
-
-		onRecording = false;
-
-		Log.Info($"[ReverseTime] Stop {index}");
-	}
-
-	protected override void OnStart()
-	{
-		Record();
-	}
-
-	protected override void OnFixedUpdate()
+	public void Record()
 	{
 		if (!onRecording) return;
 		if (TimeRecording)
 		{
-			Stop();
+			StopRecord();
 
 			return;
 		}
 
 		index++;
 
-		actions.Add(index, "a");
+		var value = GetCurrentAction();
+
+		if (!actions.TryAdd(index, value)) 
+			actions[index] = value;
 
 		Log.Info($"[ReverseTime] Index {index} | TimeRecording {TimeRecording}");
 	}
+
+	public void StopRecord()
+	{
+		if (!onRecording) return;
+
+		onRecording = false;
+
+		Log.Info($"[ReverseTime] Stop {index}");
+
+		foreach (var key in actions.Keys)
+			Log.Info($"Key {key} | Value {actions[key]}");
+	}
+
+	private string GetCurrentAction()
+	{
+		string value = "";
+
+		//? can clean this code, I didn't find any Input.GetCurrentAction()
+		if (Input.Released("use"))
+		{
+			value = "use";
+		} 
+		else if (Input.Released("attack1"))
+		{
+			value = "attack1";
+		} 
+		else if (Input.Released("attack2"))
+		{
+			value = "attack2";
+		} 
+		else if (Input.Released("Forward"))
+		{
+			value = "Forward";
+		} 
+		else if (Input.Released("Backward"))
+		{
+			value = "Backward";
+		} 
+		else if (Input.Released("Left"))
+		{
+			value = "Left";
+		} 
+		else if (Input.Released("Right"))
+		{
+			value = "Right";
+		} 
+		else if (Input.Released("Jump"))
+		{
+			value = "Jump";
+		} 
+		else if (Input.Released("Run"))
+		{
+			value = "Run";
+		} 
+		else if (Input.Released("Walk"))
+		{
+			value = "Walk";
+		} 
+		else if (Input.Released("Duck"))
+		{
+			value = "Duck";
+		}
+
+		return value;
+	}
+	#endregion
+
+	#region Play logic
+	public void StartPlay()
+	{
+
+	}
+
+	public void Play()
+	{
+
+	}
+
+	public void StopPlay()
+	{
+
+	}
+	#endregion
+
+	#region Components
+	protected override void OnStart()
+	{
+		//StartRecord(); //todo remove
+	}
+
+	protected override void OnFixedUpdate()
+	{
+		if (onRecording)
+		{
+			Record();
+		} 
+		else if (onPlaying)
+		{
+			Play();
+		}
+	}
+	#endregion
 }
