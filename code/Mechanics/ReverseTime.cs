37a121849048cf97c952ce523ad91e8ee3cdf0aa
Titanovsky <titanovskyteam@gmail.com>
2024-03-05 06:56:35 +0500

    add alpha logic ReverseTime

--- code/Mechanics/ReverseTime.cs
+++ code/Mechanics/ReverseTime.cs
@@ -1,9 +1,54 @@
-﻿public sealed class ReverseTime : Component
+﻿using System;
+
+public sealed class ReverseTime : Component
 {
 	[Property] public GameObject ply;
+
+	private bool onRecording;
+	private int index = 0;
+	private float time = 10f;
+	private TimeUntil TimeRecording { get; set; }
 
 	//todo 1. Сделать OnRecordInputsPlayers
 	//todo 2. Записывать действия игрока 20 секунд (ключ = единица времени; значение = инпут) 
 	//todo 3. Переделать PlayerController
 	//todo 4. Воспроизвести + не забыть про предметы
+
+	public void Record()
+	{
+		if (onRecording) return;
+
+		index = 0;
+		TimeRecording = time;
+		onRecording = true;
+
+		Log.Info($"[ReverseTime] Record");
+	}
+
+	public void Stop()
+	{
+		if (!onRecording) return;
+
+		onRecording = false;
+		Log.Info($"[ReverseTime] Stop {index}");
+	}
+
+	protected override void OnStart()
+	{
+		Record();
+	}
+
+	protected override void OnFixedUpdate()
+	{
+		if (!onRecording) return;
+		if (TimeRecording)
+		{
+			Stop();
+
+			return;
+		}
+
+		index++;
+		Log.Info($"[ReverseTime] index {index} | timeRecording {TimeRecording}");
+	}
 }
